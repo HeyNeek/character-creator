b16073cc9113530d78722d3b9edb1db9862b761d
David <DEvangelista54@gmail.com>
2021-11-17 02:12:52 -0500

    gallery

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Charactercard from './charactercard';
 
 function Gallery(){
+
+    const [characterList, setCharacterList] = useState([])
+useEffect(() => {
+    
+    fetch("/player_characters").then((r) => {
+      if (r.ok) {
+        r.json().then((characters) => {
+            setCharacterList(characters)
+        });
+      }
+    });
+  }, []);
+
+  const list = characterList.map((character) => <Charactercard character = {character}/>);
+
     return(
         <div>
             <h1>Gallery</h1>
+            {list}
         </div>
     )
 }
