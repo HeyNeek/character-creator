e89c36f7229b524f4dc9bf83725fa2f9864fa541
HeyNeek <45445231+HeyNeek@users.noreply.github.com>
2021-11-17 00:33:35 -0600

    More CSS, mainly for navbar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,7 +7,7 @@ export default function Home({user}){
    
     return(
     <div>
-        <h2 id="userGreeting">Hi {user.username}!<img className = "userphoto" src={user.image_url}/></h2>
+        <h2 id="userGreeting">Greetings {user.username}!<img className = "userphoto" src={user.image_url}/></h2>
         <br/>
         {/* mapping here */}
         <h1 id="currentCharactersHome"> Your current characters </h1>
