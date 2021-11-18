222457d2ce7c8cbd2fcd315af5ec94a081b099a1
David <DEvangelista54@gmail.com>
2021-11-18 00:34:15 -0500

    put in race seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Seeding Race data..."
 
-# Race.create(name: 'Dragonborn')
-# Race.create(name: 'Dwarf')
-# Race.create(name: 'Elf')
-# Race.create(name: 'Drow')
-# Race.create(name: 'Gnome')
-# Race.create(name: 'Half-Elf')
-# Race.create(name: 'Halfling')
-# Race.create(name: 'Human')
-# Race.create(name: 'Tiefling')
+Race.create(name: 'Dragonborn')
+Race.create(name: 'Dwarf')
+Race.create(name: 'Elf')
+Race.create(name: 'Drow')
+Race.create(name: 'Gnome')
+Race.create(name: 'Half-Elf')
+Race.create(name: 'Halfling')
+Race.create(name: 'Human')
+Race.create(name: 'Tiefling')
 
 # test user
 puts "Seeding User data..."
